74879419f4a43403b8ac5d68fbaf34a6f29b0f8d
Айнур <www.molnia.g@mail.ru>
2021-01-02 16:12:47 +0500

    генерация ландшавта

--- main.py
+++ main.py
@@ -49,36 +49,6 @@ def terminate():
     sys.exit()
 
 
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (500, 500))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
 tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png'), 'water': load_image('woda.jpg')}
 
 tile_width = tile_height = 50
@@ -130,7 +100,6 @@ def random_level(level1):
 level = load_level('levelex1.txt')
 level = random_level(level)
 generate_level(level)
-start_screen()
 running = True
 sprite = pygame.sprite.Sprite()
 sprite.image = load_image("dom.jpg")
